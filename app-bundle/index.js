7f491dba22f1faca39c8bec624ebb0d6632210db
urahiroshi <asj.hiroshi@gmail.com>
2019-08-01 20:45:18 +0900

    Add sub-component

--- app-bundle/index.js
+++
@@ -1,3 +0,0 @@
-import React from "react"
-
-export default () => <h2>Bunble Component</h2>
---
+++ app-bundle/src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import SubComponent from './sub-component'
+
+export default () => (
+  <div>
+    <h2>Bunble Component</h2>
+    <SubComponent />
+  </div>
+)
