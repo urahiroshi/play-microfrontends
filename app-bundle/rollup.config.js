c6c616d4a3643f46d612b907690512ca9d0bd610
urahiroshi <asj.hiroshi@gmail.com>
2019-08-01 21:43:13 +0900

    Add Rollup

---
+++ app-bundle/rollup.config.js
@@ -0,0 +1,17 @@
+import typescript from 'rollup-plugin-typescript2'
+
+export default {
+  input: './src/index.tsx',
+  output: [{
+    file: 'bundle.js',
+    name: 'appBundle',
+    format: 'umd',
+    globals: {
+      react: 'React'
+    }
+  }],
+  external: ['react', 'react-dom'],
+  plugins: [
+    typescript()
+  ]
+}
---
+++ app-bundle/sub-component.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+declare const SubComponent: () => JSX.Element;
+export { SubComponent };
