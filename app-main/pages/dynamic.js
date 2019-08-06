7cf651cc328c4d1282c4efd4c0d7395112dfb398
urahiroshi <asj.hiroshi@gmail.com>
2019-08-07 00:35:59 +0900

    Fix render-bundle-app to use CustomElements

--- app-main/pages/dynamic.js
+++ app-main/pages/dynamic.js
@@ -2,10 +2,10 @@ import Head from 'next/head'
 
 export default () => (
   <>
-    <div id="bundleApp"></div>
+    <my-bundle-app></my-bundle-app>
     <Head>
-      <script src="http://localhost:8000/bundle.js"></script>
-      <script src="/static/render-bundle-app.js" defer></script>
+      <script src="http://localhost:8000/bundle.js" async={false}></script>
+      <script src="/static/render-bundle-app.js" async={false}></script>
     </Head>
   </>
 )
--- app-main/static/render-bundle-app.js
+++ app-main/static/render-bundle-app.js
@@ -1,6 +1,22 @@
 (function () {
-  var newElem = document.createElement('div');
-  var bundleAppElem = document.getElementById('bundleApp');
-  bundleAppElem.appendChild(newElem);
-  BundleApp.render(newElem);  
+  var renderIfBundleAppDefined = function () {
+    if (typeof BundleApp === 'undefined') {
+      setTimeout(renderIfBundleAppDefined, 10);
+    } else {
+      var tagName = 'my-bundle-app';
+      var constructor = document.createElement(tagName).constructor;
+      if (constructor !== HTMLUnknownElement && constructor !== HTMLElement) {
+        return;
+      }
+      class BundleAppElement extends HTMLElement {
+        connectedCallback() {
+          var mountPoint = document.createElement('div');
+          this.attachShadow({ mode: 'open' }).appendChild(mountPoint);
+          BundleApp.render(mountPoint);
+        }
+      }
+      customElements.define(tagName, BundleAppElement);
+    }
+  }
+  renderIfBundleAppDefined();
 })();
